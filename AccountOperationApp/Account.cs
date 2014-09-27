54f64544d5dbeedad8eddf5bc6f534bbceaf9c0d
shiblyshrof <shibly_shrof@yahoo.com>
2014-09-27 16:50:36 +0600

    using set get

---
+++ AccountOperationApp/Account.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationApp
+{
+    class Account
+    {
+        private string number;
+        private string name;
+        private double balance=0;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Number
+        {
+            get { return number; }
+            set { number = value; }
+
+        }
+
+        public double Balance
+        {
+            get { return balance; }
+
+        }
+
+        public void Deposite(double a)
+        {
+            balance += a;
+            MessageBox.Show(a + " amount is deposited");
+        }
+
+        public void Withdraw(double a)
+        {
+            balance -= a;
+            MessageBox.Show(a + " amount is Withdrawn");
+        }
+
+        
+
+    }
+}
---
+++ AccountOperationApp/AccountUI.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccountOperationApp
+{
+    public partial class AccountUI : Form
+    {
+        public AccountUI()
+        {
+            InitializeComponent();
+        }
+
+        
+        Account anAccount= new Account();
+        private void createButton_Click(object sender, EventArgs e)
+        {
+            if (customerNameTxtBox.Text == string.Empty || acNumberTxtBox.Text == string.Empty)
+            {
+                MessageBox.Show("Empty Fields");
+            }
+            else
+            {
+                createButton.Enabled = false;
+                anAccount.Name = customerNameTxtBox.Text;
+                anAccount.Number = acNumberTxtBox.Text; 
+            }
+            
+        }
+
+        private void depositeButton_Click(object sender, EventArgs e)
+        {
+            if (anAccount.Name != null && anAccount.Number != null)
+            {
+                if (amountTxtBox.Text!=string.Empty)
+                {
+                    
+                    double aVariable = Convert.ToDouble(amountTxtBox.Text);
+                    anAccount.Deposite(aVariable);
+                    amountTxtBox.Text = string.Empty;
+                }
+                else
+                {
+                    MessageBox.Show("Enter an amount you want to deposite...");
+                }
+            }
+            else
+            {
+                MessageBox.Show("First you must create an account");
+            }
+        }
+
+        private void withdrawButton_Click(object sender, EventArgs e)
+        {
+            if (anAccount.Name != null && anAccount.Number != null)
+            {
+                if (amountTxtBox.Text!=string.Empty)
+                {
+                    double aVariable = Convert.ToDouble(amountTxtBox.Text);
+                    anAccount.Withdraw(aVariable);
+                    amountTxtBox.Text =string.Empty;
+                }
+                else
+                {
+                    MessageBox.Show("Enter an amount you want to withdraw...");
+                }
+            }
+            else
+            {
+                MessageBox.Show("First you must create an account");
+            }
+        }
+
+        private void reportButton_Click(object sender, EventArgs e)
+        {
+            if (createButton.Enabled == false)
+            {
+                string name = anAccount.Name;
+                string accNo = anAccount.Number;
+                double balance = anAccount.Balance;
+                MessageBox.Show(name + " your account number: " + accNo + " ant it's balance is: " + balance.ToString());
+            }
+
+            else
+            {
+                MessageBox.Show("Recheck account information");
+            }
+            
+        }
+    }
+}
